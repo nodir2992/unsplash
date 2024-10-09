ad7a2e580d6a4bd5fdba0f8befec61e5e293698f
nodir2992 <nodir2992@gmail.com>
2024-10-09 18:02:17 +0500

    login...

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -29,7 +29,7 @@ function Login() {
   }, [formData]);
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-50 dark:bg-gray-900 h-full">
       <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
         <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
           <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -39,7 +39,7 @@ function Register() {
   }, [formData]);
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="h-full bg-gray-50 dark:bg-gray-900">
       <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
         <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
           <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
